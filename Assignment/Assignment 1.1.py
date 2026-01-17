7f29990d03ff2bc5f316ef7fe304f89e5f0204c3
Nihal Mishra <nihalmishra3009@gmail.com>
2026-01-17 22:10:01 +0530

    Added Assignment folder with Assignment1&2

---
+++ Assignment/Assignment 1.1.py
@@ -0,0 +1,16 @@
+# Task 1 : Perform Basic Mathematical operations
+# PS : WAP That does the following
+# Take input form User
+# perform : Addition, Subtraction,Multiplication
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+print("Addition : ",num1+num2)
+print("Subtraction : ",num1-num2)
+print("Multiplication : ",num1*num2)
+print("Division : ",num1/num2)
+print("floor division : ",num1//num2)
+
+
---
+++ Assignment/Assignment 2.1.py
@@ -0,0 +1,10 @@
+# Task 1 : Check a Number is Even or Odd
+num = float(input("Enter a number: "))
+print("The Number is even :",num) if num % 2 == 0 else print("The Number is odd :",num)
+# Enter Number : 7
+# The Number is Odd
+# Enter Number : 8
+
+
+
+
